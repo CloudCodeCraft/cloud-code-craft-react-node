d2ca5fa9ade03c5ec102c96b4881e45cb6ab9082
Daniel Lee <dansclee@outlook.com>
2025-04-05 23:09:20 -0600

    adding mobile responsive navbar

--- app/navbar/navbar.tsx
+++ app/navbar/navbar.tsx
@@ -2,19 +2,112 @@ import Toolbar from "@mui/material/Toolbar";
 import IconButton from "@mui/material/IconButton";
 import AppBar from "@mui/material/AppBar";
 import MenuIcon from "@mui/icons-material/Menu";
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
+import Drawer from "@mui/material/Drawer";
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+import { Link } from "react-router";
+import { useState } from "react";
+
+function MediumNavbar() {
+  return (
+    <AppBar
+      position="static"
+      sx={{ display: { xs: "none", sm: "none", md: "block" } }}
+    >
+      <Toolbar>
+        <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+          <Link to={"/"}>Daniel Lee</Link>
+        </Typography>
+
+        <Box sx={{ justifyContent: "space-between" }}>
+          <Link to={"/jobExperience"}>
+            <Typography variant="h6" component="span" sx={{ mr: 3 }}>
+              Experience
+            </Typography>
+          </Link>
+          <Link to={"/blog"}>
+            <Typography variant="h6" component="span" sx={{ mr: 3 }}>
+              Blog
+            </Typography>
+          </Link>
+          <Link to={"/jobExperience"}>
+            <Typography variant="h6" component="span" sx={{ mr: 3 }}>
+              TODO
+            </Typography>
+          </Link>
+        </Box>
+      </Toolbar>
+    </AppBar>
+  );
+}
+
+function SmallNavbar() {
+  const [drawerToggle, setDrawerToggle] = useState(false);
+  return (
+    <AppBar
+      position="static"
+      sx={{ display: { xs: "block", sm: "block", md: "none" } }}
+    >
+      <Toolbar sx={{}}>
+        <IconButton
+          size="large"
+          edge="start"
+          aria-label="menu"
+          sx={{}}
+          onClick={() => setDrawerToggle(true)}
+        >
+          <MenuIcon />
+        </IconButton>
+
+        <Drawer
+          variant="temporary"
+          open={drawerToggle}
+          onClose={() => setDrawerToggle(false)}
+          ModalProps={{
+            keepMounted: true, // Better open performance on mobile.
+          }}
+          sx={{
+            "& .MuiDrawer-paper": { boxSizing: "border-box" },
+          }}
+        >
+          <List>
+            <ListItem>
+              <Link
+                to={"/jobExperience"}
+                onClick={() => setDrawerToggle(false)}
+              >
+                <Typography variant="h6" component="span" sx={{ mr: 3 }}>
+                  Experience
+                </Typography>
+              </Link>
+            </ListItem>
+
+            <ListItem>
+              <Link to={"/blog"} onClick={() => setDrawerToggle(false)}>
+                <Typography variant="h6" component="span" sx={{ mr: 3 }}>
+                  Blog
+                </Typography>
+              </Link>
+            </ListItem>
+          </List>
+        </Drawer>
+        <Box sx={{ flexGrow: 1 }}>{/* spacer */}</Box>
+
+        <Typography variant="h6" component="div" sx={{}}>
+          <Link to={"/"}>Daniel Lee</Link>
+        </Typography>
+      </Toolbar>
+    </AppBar>
+  );
+}
 
 export default function Navbar() {
   return (
     <>
-      <AppBar position="static">
-        <Toolbar>
-          <IconButton>
-            <IconButton size="large" edge="start" aria-label="menu">
-              <MenuIcon />
-            </IconButton>
-          </IconButton>
-        </Toolbar>
-      </AppBar>
+      <MediumNavbar />
+      <SmallNavbar />
     </>
   );
 }
