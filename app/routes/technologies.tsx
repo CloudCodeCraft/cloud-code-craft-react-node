e544eb912794b851cd901294f8d186fb0eb48807
Daniel Lee <CloudCodeCraft@gmail.com>
2025-07-26 12:32:48 -0400

    adding technologies

--- app/routes/technologies.tsx
+++ app/routes/technologies.tsx
@@ -1,3 +1,20 @@
-export default function TechExperience() {
-    return <div>test</div>
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+import Typography from "@mui/material/Typography";
+import SectionDivider from "~/components/job-experience/section-divider/section-divider";
+
+export default function Technologies() {
+  return (
+    <div>
+      <Typography variant="h1" sx={{textAlign: 'center'}}>Technologies</Typography>
+      <SectionDivider width="sm"></SectionDivider>
+      <List>
+        <ListItem>
+          <Typography variant="h2">Ruby On Rails</Typography>
+        </ListItem>
+              <SectionDivider width="md"></SectionDivider>
+
+      </List>
+    </div>
+  );
 }
